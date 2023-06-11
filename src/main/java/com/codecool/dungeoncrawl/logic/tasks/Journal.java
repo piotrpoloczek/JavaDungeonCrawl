33f625d718eea472d1528d4f1df3849e324f658f
Piotr Poloczek <piotr.jan.poloczek@gmail.com>
2023-06-11 19:35:06 +0200

    changes

--- src/main/java/com/codecool/dungeoncrawl/logic/tasks/Journal.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/tasks/Journal.java
@@ -3,8 +3,6 @@ package com.codecool.dungeoncrawl.logic.tasks;
 import java.util.List;
 
 public class Journal {
-
     private List<Task> actualTasks;
     private List<Task> finishedTask;
-
 }
--- src/main/java/com/codecool/dungeoncrawl/logic/tasks/Task.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/tasks/Task.java
@@ -5,11 +5,8 @@ import com.codecool.dungeoncrawl.logic.gameobject.items.Item;
 import java.util.List;
 
 public abstract class Task {
-
     private String name;
     private List<Item> reward;
     private String aim;
     private boolean finished;
-
-
 }
