9645e536c47252349ab159fee9bed64b8208fb8d
piotr <piotr@danielewicz.eu>
2023-06-11 13:54:33 +0200

    food power

--- src/main/java/com/codecool/dungeoncrawl/logic/gameobject/items/food/Food.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/gameobject/items/food/Food.java
@@ -2,12 +2,15 @@ package com.codecool.dungeoncrawl.logic.gameobject.items.food;
 
 import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.gameobject.items.Item;
+import lombok.Getter;
 
 public abstract class Food extends Item {
+    @Getter
+    private int itemPower;
 
-    public Food(Cell cell) {
+    public Food(Cell cell, int itemPower) {
         super(cell);
+        this.itemPower = itemPower;
     }
-
 
 }
