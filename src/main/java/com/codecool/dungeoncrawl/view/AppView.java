017a06b27c07332216ada9ebede337577f51ff15
Piotr Poloczek <piotr.jan.poloczek@gmail.com>
2023-06-12 23:56:44 +0200

    implement third thread for display

--- src/main/java/com/codecool/dungeoncrawl/view/AppView.java
+++ src/main/java/com/codecool/dungeoncrawl/view/AppView.java
@@ -1,10 +1,13 @@
 package com.codecool.dungeoncrawl.view;
 
 import com.codecool.dungeoncrawl.logic.Direction;
+import com.codecool.dungeoncrawl.logic.exceptions.NewLevelException;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+import lombok.Getter;
+import lombok.Setter;
 
 
 public class AppView extends Application {
@@ -19,6 +22,8 @@ public class AppView extends Application {
     private Scene classMenuScene;
     private Scene gameScene;
     private Scene inventoryScene;
+    @Getter @Setter
+    private Thread displayThread;
 
 
     public static void main(String[] args) {
@@ -35,6 +40,24 @@ public class AppView extends Application {
         this.inventoryMenuView = new InventoryMenuView(this, gameView.getGame());
         this.inventoryScene = createInventoryScene();
         showMainMenuView();
+        this.displayThread = createDisplayThread();
+    }
+
+    private Thread createDisplayThread() {
+        Thread displayThread = new Thread(() -> {
+            while (true) {
+                System.out.println("it we cow");
+                    gameView.refreshView();
+
+                // Delay between turns (optional)
+                try {
+                    Thread.sleep(100); // Adjust the delay as needed
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return displayThread;
     }
 
     private Scene createMainMenuScene() {
@@ -87,6 +110,8 @@ public class AppView extends Application {
         primaryStage.setScene(gameScene);
         gameView.refreshView();
         primaryStage.show();
+
+        displayThread.start();
     }
 
 
