a5a5765c3cf2ad8f669da2856c88814c1be1a9a9
Michal Madej <mkmadej@interia.pl>
2023-06-13 20:46:46 +0200

    MapLoader refactor #2, few small changes to decrease code amount

--- src/main/java/com/codecool/dungeoncrawl/logic/map/MapLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/map/MapLoader.java
@@ -22,17 +22,19 @@ import java.util.*;
 public class MapLoader {
 
     private static Map<Character, String> symbolsClasses = new HashMap<>();
+    private static final String GAME_OBJECT_PACKAGE_PATH = "com.codecool.dungeoncrawl.logic.gameobject.";
 
     static {
-        symbolsClasses.put('s' , "com.codecool.dungeoncrawl.logic.gameobject.actors.monsters.Skeleton");
-        symbolsClasses.put('@' , "com.codecool.dungeoncrawl.logic.gameobject.actors.player.Player");
-        symbolsClasses.put('d' , "com.codecool.dungeoncrawl.logic.gameobject.specialitems.stairs.StairsDown");
-        symbolsClasses.put('w' , "com.codecool.dungeoncrawl.logic.gameobject.actors.npc.Wizard");
-        symbolsClasses.put('k' , "com.codecool.dungeoncrawl.logic.gameobject.items.keys.RedKey");
-        symbolsClasses.put('g' , "com.codecool.dungeoncrawl.logic.gameobject.specialitems.doors.RedDoor");
-        symbolsClasses.put('$' , "com.codecool.dungeoncrawl.logic.gameobject.items.treasures.Gold");
-        symbolsClasses.put('h' , "com.codecool.dungeoncrawl.logic.gameobject.specialitems.stairs.StairsHeaven");
+        symbolsClasses.put('s' , GAME_OBJECT_PACKAGE_PATH + "actors.monsters.Skeleton");
+        symbolsClasses.put('@' , GAME_OBJECT_PACKAGE_PATH + "actors.player.Player");
+        symbolsClasses.put('d' , GAME_OBJECT_PACKAGE_PATH + "specialitems.stairs.StairsDown");
+        symbolsClasses.put('w' , GAME_OBJECT_PACKAGE_PATH + "actors.npc.Wizard");
+        symbolsClasses.put('k' , GAME_OBJECT_PACKAGE_PATH + "items.keys.RedKey");
+        symbolsClasses.put('g' , GAME_OBJECT_PACKAGE_PATH + "specialitems.doors.RedDoor");
+        symbolsClasses.put('$' , GAME_OBJECT_PACKAGE_PATH + "items.treasures.Gold");
+        symbolsClasses.put('h' , GAME_OBJECT_PACKAGE_PATH + "specialitems.stairs.StairsHeaven");
     }
+
     public static GameMap loadMap(String filename, Player player) {
         InputStream is = MapLoader.class.getResourceAsStream(filename);
         Scanner scanner = new Scanner(is);
@@ -49,38 +51,22 @@ public class MapLoader {
             for (int x = 0; x < width; x++) {
                 if (x < line.length()) {
                     Cell cell = map.getCell(x, y);
+
                     char currentChar = line.charAt(x);
-                    if(currentChar == ' ') {
-                        cell.setType(CellType.EMPTY);
-                    }
-                    else if(currentChar == '#') {
-                        cell.setType(CellType.WALL);
-                    }
-                    else {
-                        try {
-                            cell.setType(CellType.FLOOR);
-                            if(symbolsClasses.get(currentChar) == null) {
-                                continue;
-                            }
-                            String className = symbolsClasses.get(currentChar);
+                    setCellTypeByChar(currentChar, cell);
 
-                            Class aClass = Class.forName(className);
+                    if (currentChar != ' ' && currentChar != '#') {
+                        if(symbolsClasses.get(currentChar) == null) {
+                            continue;
+                        }
+                        String className = symbolsClasses.get(currentChar);
+                        Object o = createObjectByClassName(className, cell);
 
-                            Class[] types = {Cell.class};
-                            Constructor constructor = aClass.getConstructor(types);
-
-                            Object[] parameters = {cell};
-                            Object o = constructor.newInstance(parameters);
-
-                            if(currentChar == 's') {
-                                monsters.add((Monster) o);
-                            }
-                            if(currentChar == '@') {
-                                map.getPlayer().setCell(cell);
-                            }
-
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                        if(currentChar == 's') {
+                            monsters.add((Monster) o);
+                        }
+                        if(currentChar == '@') {
+                            map.getPlayer().setCell(cell);
                         }
                     }
                 }
@@ -90,6 +76,35 @@ public class MapLoader {
         map.setMonsters(monsters);
 
         return map;
+    }
+
+    private static Object createObjectByClassName(String className, Cell cell) {
+
+        try {
+            Class aClass = Class.forName(className);
+
+            Class[] types = {Cell.class};
+            Constructor constructor = aClass.getConstructor(types);
+
+            Object[] parameters = {cell};
+            return constructor.newInstance(parameters);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static void setCellTypeByChar(char symbol, Cell cell) {
+        if(symbol == ' ') {
+            cell.setType(CellType.EMPTY);
+        }
+        else if(symbol == '#') {
+            cell.setType(CellType.WALL);
+        }
+        else {
+            cell.setType(CellType.FLOOR);
+        }
     }
 
 }
