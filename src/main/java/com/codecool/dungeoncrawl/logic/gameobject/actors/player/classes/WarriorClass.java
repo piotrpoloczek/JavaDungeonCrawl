b0928b65c677aedc5145c198a0057f5acbb04271
Piotr Poloczek <piotr.jan.poloczek@gmail.com>
2023-06-13 15:53:56 +0200

    add structure for attribute classes

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/gameobject/actors/player/classes/WarriorClass.java
@@ -0,0 +1,19 @@
+package com.codecool.dungeoncrawl.logic.gameobject.actors.player.classes;
+
+import com.codecool.dungeoncrawl.logic.gameobject.actors.attributes.Dexterity;
+import com.codecool.dungeoncrawl.logic.gameobject.actors.attributes.Health;
+import com.codecool.dungeoncrawl.logic.gameobject.actors.attributes.Strength;
+
+public class WarriorClass extends Class {
+
+    private static Health HEALTH = new Health(10);
+    private static Strength STRENGTH = new Strength();
+    private static Dexterity DEXTERITY = new Dexterity();
+
+
+    public WarriorClass() {
+        super();
+        // set health, strength and dexterity based on the choosen class
+    }
+
+}
